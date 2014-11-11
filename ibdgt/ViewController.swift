237a0ff1a67bfe21b5fddb0066fb351ec2c6be5d
umetzu <dennis@umetzu.com>
2014-11-11 11:15:27 -0500

    Project Created
    First Commit

---
+++ ibdgt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ibdgt
+//
+//  Created by Baker on 11/11/14.
+//  Copyright (c) 2014 Umetzu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
